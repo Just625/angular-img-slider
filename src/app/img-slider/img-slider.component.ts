9cba55c8a4a7a8e587cf07e05a4f42b3aef053c8
Just625 <bloodseekerofdota@gmail.com>
2021-07-29 20:41:59 +0700

    [Bài tập] Ứng dụng Image Slide

---
+++ src/app/img-slider/img-slider.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-img-slider',
+  templateUrl: './img-slider.component.html',
+  styleUrls: ['./img-slider.component.css']
+})
+export class ImgSliderComponent implements OnInit {
+  imgList: string[] = ['https://via.placeholder.com/1280x420/e91e63/ffffff?text=1',
+    'https://via.placeholder.com/1280x420/e91e63/ffffff?text=2',
+    'https://via.placeholder.com/1280x420/e91e63/ffffff?text=3',
+    'https://via.placeholder.com/1280x420/e91e63/ffffff?text=4'];
+  currentSrc = 0;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  changePicture(numb: number) {
+    this.currentSrc = numb;
+  }
+}
